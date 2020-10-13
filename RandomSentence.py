7320ab552d8d10422d19dfeff31e656472991e64
deborah brouwer <3563deb6753@gmail.com>
2020-10-12 21:21:57 -0700

    Add GUI Display

--- RandomSentence.py
+++ RandomSentence.py
@@ -1,8 +1,10 @@
 """
-This program generates 10 random sentences from a text file.
+This program generates random sentences from a text file.
+And displays them in a graphical window
 """
 import random
 import tkinter.filedialog
+from functools import partial 
 
 # This function reads a file into a list of strings.
 # String elements are separated by the newline character.
@@ -13,26 +15,53 @@ def readFile(filename):
     text = readFile.readlines()
     readFile.close()
     return text
-    
-# This function gets a random sentence.
-# @text is a list of strings
-# Returns a string.
-def getSentence(text):
-    randomNum = random.randint(1, len(text)-1)
-    return text[randomNum]
 
-def main():
+    
+# This function gets a random sentence within a normal size range.
+# @text is a list of strings
+# @display_text is a tkinter variable string that holds the label text
+def getSentence(text, display_text):
+    sentence=""
+    while len(sentence) < 50 or len(sentence) > 281:
+        randomNum = random.randint(1, len(text)-1)
+        sentence = text[randomNum]
+    display_text.set(sentence) #set the sentence as the window label's text
+
+
+def createWindow():
+    my_window = tkinter.Tk()
+    my_window.geometry("1400x280")
+    my_window.title("")
+    
+    my_button = tkinter.Button(my_window,text="again")
+    my_button.config(font=("Courier", 24))
+    my_button.pack() 
+    
+    display_text = tkinter.StringVar(value="default")
+    my_Label = tkinter.Label(my_window, textvariable=display_text)
+    my_Label.config(wraplength=1300)
+    my_Label.config(font=("Courier", 24))
+    my_Label.pack()
+    
+    return my_window, display_text, my_button
+
+def getFile():
     try:
-        root = tkinter.Tk()
-        root.withdraw()
         filename = tkinter.filedialog.askopenfilename()
+        return filename
     except IOError:
         print("file not found")
-    else:
-        text = readFile(filename)
-        for x in range(10):  # number of random sentences generated
-            sentence = getSentence(text)
-            if len(sentence) >50 and len(sentence) < 281: 
-                print(sentence)
+
+def main():
+    my_window, display_text, my_button = createWindow() 
+    my_window.withdraw() #hide the window while getting the file
+    filename = getFile()
+    text = readFile(filename)
+    getSentence(text, display_text)
+    my_button.config(command=partial(getSentence, text, display_text))
+    my_window.deiconify() #show the window
+    my_window.mainloop() #keep the window until closed by user
+ 
+
     
 main()
